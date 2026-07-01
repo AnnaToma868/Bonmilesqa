c2f246e12cf4fd79bfd4611c7b65e9e71d91fbc4
Anna Toma <annatooma37@gmail.com>
2026-07-01 12:00:17 +0300

    Reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,7 +2,7 @@ public class BonusMilesService {
     public int calculate(int price) {
         int result;
         if (price > 19) {
-            result = price/20 ;
+            result = price / 20;
         } else {
             result = 0;
         }
